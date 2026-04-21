14ed150dea56bdc0b3b8ded588f7223f806f6447
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-04-21 16:14:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,191 +1,233 @@
 const axios = require("axios");
-const FormData = require("form-data");
+const fs = require("fs");
 
 module.exports = class Seedr {
-  constructor() {
-    this.token = null;
-  }
+  constructor(email, password) {
+    this.email = email;
+    this.password = password;
 
-  async login(username, password) {
-    const data = new FormData();
-
-    data.append("grant_type", "password");
-    data.append("client_id", "seedr_chrome");
-    data.append("type", "login");
-    data.append("username", username);
-    data.append("password", password);
-
-    const res = await axios.post(
-      "https://www.seedr.cc/oauth_test/token.php",
-      data,
-      { headers: data.getHeaders() }
-    );
-
-    this.token = res.data.access_token;
-    this.rft = res.data.refresh_token;
-
-    return this.token;
-  }
-
-  async addMagnet(magnet) {
-    const data = new FormData();
-
-    data.append("access_token", this.token);
-    data.append("func", "add_torrent");
-    data.append("torrent_magnet", magnet);
-
-    const res = await axios.post(
-      "https://www.seedr.cc/oauth_test/resource.php",
-      data,
-      { headers: data.getHeaders() }
-    );
-
-    return res.data;
-  }
-
-  // 🚀 FAST VERSION (FIXED)
-  async getVideos() {
-    const root = await axios.get(
-      `https://www.seedr.cc/api/folder?access_token=${this.token}`
-    );
-
-    const folders = root.data.folders || [];
-
-    const results = await Promise.all(
-      folders.map(async (folder) => {
-        const res = await axios.get(
-          `https://www.seedr.cc/api/folder/${folder.id}?access_token=${this.token}`
-        );
-
-        return (res.data.files || [])
-          .filter(f => f.play_video)
-          .map(f => ({
-            fid: folder.id,
-            id: f.folder_file_id,
-            name: f.name
-          }));
-      })
-    );
-
-    return results.flat();
-  }
-
-  // 🚀 FULL FILE LIST (FAST + CLEAN)
-  async getFilesById(id = null) {
-    const url = id
-      ? `https://www.seedr.cc/api/folder/${id}?access_token=${this.token}`
-      : `https://www.seedr.cc/api/folder?access_token=${this.token}`;
-
-    const data = await axios.get(url);
-
-    const d = data.data;
-
-    const res = {
-      parentId: d.parent !== -1 ? d.parent : null,
-      name: d.name,
-      folderSize: 0,
-      totalStorage: d.space_max,
-      usedStorage: d.space_used,
-      type: d.type,
-      files: [],
-      activeTorrents: d.torrents
+    this.auth = {
+      auth: {
+        username: email,
+        password: password
+      }
     };
-
-    for (const folder of d.folders || []) {
-      res.files.push({
-        id: folder.id,
-        type: "folder",
-        name: folder.name,
-        size: folder.size
-      });
-
-      if (folder.size) {
-        res.folderSize += parseInt(folder.size);
-      }
-    }
-
-    for (const file of d.files || []) {
-      res.files.push({
-        id: file.folder_file_id,
-        type: "file",
-        name: file.name,
-        size: file.size
-      });
-
-      if (file.size) {
-        res.folderSize += parseInt(file.size);
-      }
-    }
-
-    return res;
   }
 
-  async getFile(id) {
-    const data = new FormData();
-
-    data.append("access_token", this.token);
-    data.append("func", "fetch_file");
-    data.append("folder_file_id", id);
-
-    const res = await axios.post(
-      "https://www.seedr.cc/oauth_test/resource.php",
-      data,
-      { headers: data.getHeaders() }
+  // =========================
+  // USER
+  // =========================
+  async getUser() {
+    const res = await axios.get(
+      "https://www.seedr.cc/rest/user",
+      this.auth
     );
 
     return res.data;
   }
 
-  async rename(id, newName) {
-    const data = new FormData();
+  // =========================
+  // FOLDERS
+  // =========================
+  async getRootFolder() {
+    const res = await axios.get(
+      "https://www.seedr.cc/rest/folder",
+      this.auth
+    );
 
-    data.append("access_token", this.token);
-    data.append("func", "rename");
-    data.append("rename_to", newName);
-    data.append("file_id", id);
+    return res.data;
+  }
 
+  async getFolder(id) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/folder/${id}`,
+      this.auth
+    );
+
+    return res.data;
+  }
+
+  async downloadFolderZip(id, outputPath) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/folder/${id}/download`,
+      {
+        ...this.auth,
+        responseType: "stream"
+      }
+    );
+
+    const writer = fs.createWriteStream(outputPath);
+
+    res.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
+  }
+
+  async createFolder(path) {
     const res = await axios.post(
-      "https://www.seedr.cc/oauth_test/resource.php",
-      data,
-      { headers: data.getHeaders() }
+      "https://www.seedr.cc/rest/folder",
+      new URLSearchParams({ path }),
+      this.auth
+    );
+
+    return res.data;
+  }
+
+  async renameFolder(id, name) {
+    const res = await axios.post(
+      `https://www.seedr.cc/rest/folder/${id}/rename`,
+      new URLSearchParams({ rename_to: name }),
+      this.auth
     );
 
     return res.data;
   }
 
   async deleteFolder(id) {
-    const data = new FormData();
-
-    data.append("access_token", this.token);
-    data.append("func", "delete");
-    data.append(
-      "delete_arr",
-      JSON.stringify([{ type: "folder", id }])
+    const res = await axios.delete(
+      `https://www.seedr.cc/rest/folder/${id}`,
+      this.auth
     );
 
+    return res.data;
+  }
+
+  // =========================
+  // FILES
+  // =========================
+  async downloadFile(fileId, outputPath) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/file/${fileId}`,
+      {
+        ...this.auth,
+        responseType: "stream"
+      }
+    );
+
+    const writer = fs.createWriteStream(outputPath);
+
+    res.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
+  }
+
+  async getFileHLS(fileId) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/file/${fileId}/hls`,
+      this.auth
+    );
+
+    return res.data;
+  }
+
+  async getFileImage(fileId) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/file/${fileId}/image`,
+      {
+        ...this.auth,
+        responseType: "stream"
+      }
+    );
+
+    return res.data;
+  }
+
+  async getThumbnail(fileId, outputPath) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/file/${fileId}/thumbnail`,
+      {
+        ...this.auth,
+        responseType: "stream"
+      }
+    );
+
+    const writer = fs.createWriteStream(outputPath);
+
+    res.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+      writer.on("finish", resolve);
+      writer.on("error", reject);
+    });
+  }
+
+  async renameFile(id, name) {
     const res = await axios.post(
-      "https://www.seedr.cc/oauth_test/resource.php",
-      data,
-      { headers: data.getHeaders() }
+      `https://www.seedr.cc/rest/file/${id}/rename`,
+      new URLSearchParams({ rename_to: name }),
+      this.auth
     );
 
     return res.data;
   }
 
   async deleteFile(id) {
-    const data = new FormData();
-
-    data.append("access_token", this.token);
-    data.append("func", "delete");
-    data.append(
-      "delete_arr",
-      JSON.stringify([{ type: "file", id }])
+    const res = await axios.delete(
+      `https://www.seedr.cc/rest/file/${id}`,
+      this.auth
     );
 
+    return res.data;
+  }
+
+  // =========================
+  // TRANSFERS
+  // =========================
+  async addMagnet(magnet) {
     const res = await axios.post(
-      "https://www.seedr.cc/oauth_test/resource.php",
-      data,
-      { headers: data.getHeaders() }
+      "https://www.seedr.cc/rest/transfer/magnet",
+      new URLSearchParams({ magnet }),
+      this.auth
+    );
+
+    return res.data;
+  }
+
+  async addURL(url) {
+    const res = await axios.post(
+      "https://www.seedr.cc/rest/transfer/url",
+      new URLSearchParams({ url }),
+      this.auth
+    );
+
+    return res.data;
+  }
+
+  async addFile(filePath) {
+    const form = new FormData();
+    form.append("file", fs.createReadStream(filePath));
+
+    const res = await axios.post(
+      "https://www.seedr.cc/rest/transfer/file",
+      form,
+      {
+        ...this.auth,
+        headers: form.getHeaders()
+      }
+    );
+
+    return res.data;
+  }
+
+  async getTransfer(id) {
+    const res = await axios.get(
+      `https://www.seedr.cc/rest/transfer/${id}`,
+      this.auth
+    );
+
+    return res.data;
+  }
+
+  async deleteTransfer(id) {
+    const res = await axios.delete(
+      `https://www.seedr.cc/rest/transfer/${id}`,
+      this.auth
     );
 
     return res.data;
